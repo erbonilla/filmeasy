2a07461210614c49283d10f55b3d03a694d51a04
erbonilla <erbonilla@outlook.com>
2024-12-19 17:38:40 -0600

    add NavBar, Movies, Actors, and MovieInformation components; update App structure with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Movies from './components/Movies/Movies';
+import Actors from './components/Actors/Actors';
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <CssBaseline />
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route path='/' element={<Movies/>} />
+                    <Route path='/actors/:id' element={<Actors/>} />
+
+                </Routes>
+            </BrowserRouter>             
         </div>
     );
 };
