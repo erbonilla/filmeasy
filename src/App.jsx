1862cae2214dbf90501042ca0636d0ee522ee729
erbonilla <erbonilla@outlook.com>
2024-12-19 20:39:48 -0600

    add Profile and MovieInformation components; enhance NavBar with responsive design and theming; implement global styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,28 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Movies from './components/Movies/Movies';
 import Actors from './components/Actors/Actors';
 import NavBar from './components/NavBar/NavBar';
+import MovieInformation from './components/MovieInformation/MovieInformation';
+import Profile from './components/Profile/Profile';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <div>
+        <div className={classes.root}>
             <CssBaseline />
             <BrowserRouter>
                 <NavBar />
+                <main className={classes.content}>
+                    <div className={classes.toolbar}/>
                 <Routes>
+                    <Route path='/movie/:id' element={<MovieInformation/>} />
                     <Route path='/' element={<Movies/>} />
                     <Route path='/actors/:id' element={<Actors/>} />
-
+                    <Route path='/profile/:id' element={<Profile/>} />
+                    <Route path='*' element={<h1>Not Found</h1>} />
                 </Routes>
+                </main>
             </BrowserRouter>             
         </div>
     );
---
+++ src/components/NavBar/navstyles.jsx
@@ -0,0 +1,24 @@
+import { makeStyles } from "@mui/styles";
+
+// const drawerWidth = 240;
+
+export default makeStyles((theme) => ({
+    toolbar: {
+        display: 'flex',
+        height: '80px',
+        justifyContent: 'space-between',
+        marginLeft: '240px',
+        [theme.breakpoints.down('sm')]: {
+            marginLeft: '0',
+            flexWrap: 'wrap'
+        }
+    },
+   menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]:{
+            display: 'none'
+        }
+
+   }
+
+}));
