d0cba0fed502aaa88b58982f243e590c926d52d4
erbonilla <erbonilla@outlook.com>
2024-12-20 08:17:55 -0600

    add categories to Sidebar component and update styles for logo alignment

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,9 +3,17 @@ import { Link } from 'react-router-dom';
 import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress, } from '@mui/material';
 import { useTheme } from '@mui/styles';
 import useStyles from './sidestyles';
+import { Label } from '@mui/icons-material';
 
 const redLogo = 'https://i.ibb.co/ft4skBS/logored.png'
 const blueLogo = 'https://i.ibb.co/5K6vdzx/logoblue.png'
+
+const categories = [
+    { Label: 'Popular', value: 'popular' },
+    { Label: 'Top Rated', value: 'top_rated' }
+    { Label: 'Upcoming', value: 'upcoming' }
+
+]
 
 const Sidebar = ({setMobileOpen}) => {
     const theme = useTheme();
--- src/components/Sidebar/sidestyles.js
+++ src/components/Sidebar/sidestyles.js
@@ -3,8 +3,13 @@ import { makeStyles } from '@mui/styles';
 const drawerWidth = 240;
 
 export default makeStyles((theme) => ({
-  toolbar: {
-    
+  imageLink: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '10%',
+  },
+  image: {
+    width: '70%',
   },
   
 }));
