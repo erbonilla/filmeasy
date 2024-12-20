9e5b489e31ff8780072034444da3110beb85d6b8
erbonilla <erbonilla@outlook.com>
2024-12-20 06:48:19 -0600

    add logo image to Sidebar component with theme-based styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress, } from '@mui/material';
 import { useTheme } from '@mui/styles';
 import useStyles from './sidestyles';
+
+const redLogo = 'https://i.ibb.co/ft4skBS/logored.png'
+const blueLogo = 'https://i.ibb.co/5K6vdzx/logoblue.png'
 
 const Sidebar = ({setMobileOpen}) => {
     const theme = useTheme();
@@ -9,6 +13,12 @@ const Sidebar = ({setMobileOpen}) => {
     return (
         <>
         <Link to="/" className={classes.imagelink}>
+        <img 
+        className={classes.image}
+        src={ theme.palette.mode === 'light' ? blueLogo : redLogo } 
+        alt='EasyLogo' />
+
+
         </Link>
         </>
     );
