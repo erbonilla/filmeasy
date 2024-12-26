379574179fd7aada690a0a1a941ab0ac398096b3
erbonilla <erbonilla@outlook.com>
2024-12-26 06:25:28 -0600

    Implement movie details fetching in MovieInformation component; add useGetMovieQuery for API integration and update imports in MovieInformation.jsx. Enhance TMDB service with getMovie query for detailed movie data retrieval.

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Modal, Box, Typography, Button, ButtonGroup, Grid, CircularProgress, useMediaQuery, Rating } from '@mui/material';
-import { Movie as MovieIcon, Theaters, Language, Favorite, PlusOne, FavoriteBorderOutlined, Remove, ArrowBack } from '@mui/icons-material';
-import {Link, useParams} from 'react-router-dom';
+import { Modal, Typography, Button, ButtonGroup, Grid, Box, CircularProgress, useMediaQuery, Rating } from '@mui/material';
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOulined, Remove, ArrowBack } from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import { useGetMovieQuery } from '../../services/TMDB';
 
 const MovieInformation = () => {
 
     const { id } = useParams();
+    const { data, isFetching, error } = useGetMovieQuery(id);
+    console.log(data);
 
 
     return (
