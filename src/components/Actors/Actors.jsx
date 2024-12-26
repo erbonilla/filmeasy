a14b36e9ff31563339c0a51955d0f9b4c7a8e402
erbonilla <erbonilla@outlook.com>
2024-12-26 14:05:39 -0600

    Refactor Actors component to include routing and styling; update header text and add actor styles. Enhance MovieInformation component by adjusting grid item sizes for better layout and responsiveness.

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Box, Button, CircularProgress, Grid, Typography }  from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+import useStyles from './actorstyles';
+import { useNavigate, useParams } from 'react-router-dom';
+ 
 
 const Actors = () => {
+    const { id } = useParams();
+    console.log(id);
+    const classes = useStyles();
+
+
+
     return (
         <div>
-           <h1>Actors Page</h1> 
+            <h1>Actros Page </h1>
         </div>
     );
 };
 
-export default Actors;
+export default Actors; 
